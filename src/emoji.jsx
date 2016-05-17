be30e7fc3586045acdd631c2abd09ac1367435de
Fravic <fravicf@gmail.com>
2016-05-17 15:06:52 -0700

    Hack to load emoji locally instead of from CDN

--- src/emoji.jsx
+++ src/emoji.jsx
@@ -1,23 +1,33 @@
 var React = require("react");
 var emojione = require("emojione");
 
+function replaceEmojiUrls(messageHTML) {
+  // Chrome packaged app CSP directive img-src disables external image loading
+  // Load emoji locally instead
+  return messageHTML.replace(
+    '//cdn.jsdelivr.net/emojione/assets/png/',
+    '/img/emoji/'
+  )
+}
+
 var Emoji = React.createClass({
   propTypes: {
     onClick: React.PropTypes.func
   },
-  
+
   shouldComponentUpdate: function(nextProps, nextState) {
     // avoid rerendering the Emoji component if the shortname hasnt changed
     return nextProps.shortname != this.props.shortname;
   },
-  
+
   createMarkup: function() {
-    return {__html: emojione.shortnameToImage(this.props.shortname)};
+    let imageHTML = replaceEmojiUrls(emojione.shortnameToImage(this.props.shortname))
+    return {__html: imageHTML};
   },
-  
+
   render: function() {
-    return <div {...this.props} onClick={this.props.onClick} tabIndex="0" className="emoji" 
-                title={this.props.name} 
+    return <div {...this.props} onClick={this.props.onClick} tabIndex="0" className="emoji"
+                title={this.props.name}
                 dangerouslySetInnerHTML={this.createMarkup()}>
     </div>
   }
